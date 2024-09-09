f4315ca2d495a005190a74222997300f49056ce3
Ayox <90204576+ayoxdev@users.noreply.github.com>
2024-09-09 14:34:34 +0200

    .

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
         MONGODB_URI:process.env.MONGODB_URI,
         MONGODB_DB:process.env.MONGODB_DB,
         CLOUDINARY_URL:process.env.CLOUDINARY_URL
-        NODE_OPTIONS=--openssl-legacy-provider
     }
 }
     
